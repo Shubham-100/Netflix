c9bc681768053129c756608c57a8d064daa08647
Shubham <aryan100jangid@gmail.com>
2023-07-14 22:07:21 +0530

    Move duplicate code to a seperate modal component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,11 +2,16 @@
 import React, { useEffect, useState } from 'react';
 import { FaPlay } from "react-icons/fa";
 import { InformationCircleIcon } from '@heroicons/react/solid';
+import BasicModal from './BasicModal';
+import YoutubeTrailer from './Youtube';
 import requests from "../utils/requests";
 
 function Banner({netflixOriginals}) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {setOpen(true);}
+  const handleClose = () => setOpen(false);
+
   const [movie, setMovie] = useState(null);
-  // console.log(netflixOriginals?.results)
   var idx = 10;
   
   useEffect(() => {
@@ -24,7 +29,6 @@ function Banner({netflixOriginals}) {
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
         <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
             <img
-                    //src="https://image.tmdb.org/t/p/original/nb9fc9INMg8kQ8L7sE7XTNsZnUX.jpg"
                     src={`${requests.url}${movie.backdrop_path || movie.poster_path}`}
                     alt={`${requests.url}${movie.title}`}
                     className="object-cover"
@@ -38,7 +42,10 @@ function Banner({netflixOriginals}) {
       <p className='max-w-xs text-xs text-shadow-md md:max-w-lg md:text-md lg:max-w-2xl lg:text-2xl'>{movie?.overview}</p>
 
       <div className="flex space-x-4 items-center gap-x-2">
-        <button className="btn bg-white"><FaPlay className='h-4 w-4 md:h-7 md:w-7'/>Play</button>
+        <button className="btn bg-white" onClick={handleOpen}><FaPlay className='h-4 w-4 md:h-7 md:w-7'/>Play</button>
+        <BasicModal open={open} onClose={handleClose}>
+          <YoutubeTrailer id={movie.id} media={movie.media_type} />
+        </BasicModal>
         <button className="btn bg-[gray]/70 text-white"><InformationCircleIcon className='h-4 w-4 md:h-7 md:w-7'/>More Info</button>
       </div>
       
--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -1,10 +1,5 @@
 // import React, { useState } from 'react'
 import * as React from 'react';
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Typography from '@mui/material/Typography';
 import BasicModal from './BasicModal';
 import YoutubeTrailer from './Youtube';
 
@@ -14,23 +9,8 @@ function Poster({movie}) {
   const handleOpen = () => {
     // console.log(movie)
     setOpen(true);
-    
   }
   const handleClose = () => setOpen(false);
-
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: "60%",
-    height: "50%",
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
 
   return (
     <div className='h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:hover:scale-110 md:h-34 md:min-w-[220px]'>
@@ -40,33 +20,9 @@ function Poster({movie}) {
           onClick={handleOpen}
       />
     
-    {/* <BasicModal open={open}/> */}
-    <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            open={open}
-            onClose={handleClose}
-            closeAfterTransition
-            slots={{ backdrop: Backdrop }}
-            slotProps={{
-              backdrop: {
-                timeout: 500,
-              },
-            }}
-      >
-            <Fade in={open}>
-              <Box sx={style} className="text-black">
-                {/* <Typography id="transition-modal-title" variant="h6" component="h2">
-                  Text in a modal
-                </Typography>
-                <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                  Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography> */}
-                
-                <YoutubeTrailer id={movie.id} media={movie.media_type}/>
-              </Box>
-            </Fade>
-      </Modal>
+      <BasicModal open={open} onClose={handleClose}>
+        <YoutubeTrailer id={movie.id} media={movie.media_type} />
+      </BasicModal>
     </div>
   )
 }

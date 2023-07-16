4d1bb82495a963fd76e148f980dd9d240dc4740f
Shubham <aryan100jangid@gmail.com>
2023-07-16 11:18:40 +0530

    Fix footer padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div><h1 className='text-center pb-5'>Copyright© 2023 | Designed and Coded by Shubham</h1> </div>
+    <div><h1 className='text-center text-sm md:text-lg py-5'>Copyright© 2023 | Designed and Coded by Shubham</h1> </div>
   )
 }
 
